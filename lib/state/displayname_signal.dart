76bc3b2b2bf4a157833cf297c662ac9a1a50e018
plotsklapps <plotsklapps@gmail.com>
2024-03-11 15:27:29 +0100

    - Trying to work out displayName bug.

--- lib/state/displayname_signal.dart
+++ lib/state/displayname_signal.dart
@@ -1,23 +1,16 @@
 import 'package:beatboks/firebase/firebase_service.dart';
 import 'package:beatboks/state/sneakpeek_signal.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:signals/signals.dart';
 
 // Signal for the displayName. We use this in the updateDisplayName method.
-final Signal<String> sDisplayName = signal<String>('New Boxer');
+final Signal<String> sDisplayName = signal<String>(
+  FirebaseService().displayName ?? 'New Boxer',
+);
 
 // Computed signal to watch in the UI.
-final Computed<String> cDisplayName = computed<String>(() {
-  // Fetch a user.
-  final User? user = FirebaseService().currentUser;
-
-  // Check if the user is a sneak peeker.
+final Computed<String> cDisplayName = computed(() {
   if (sSneakPeek.value) {
     return 'Sneak Peeker';
-  } else {
-    // If not, he can't be anything else than a Firebase user.
-    // So return the value from Firebase OR the default value from
-    // the signal we created before.
-    return user?.displayName ?? sDisplayName.value;
   }
+  return sDisplayName.value;
 });
