a623fca6ce515c9df98d83c89d812337e20c728a
plotsklapps <plotsklapps@gmail.com>
2024-03-20 18:17:49 +0100

    - Edited homescreen again, removed appbar; - Added Scrollconfiguration to read all input devices.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,165 +1,199 @@
+import 'dart:ui';
+
 import 'package:beatboks/modals/settings_bottomsheet.dart';
 import 'package:beatboks/state/checkedsongs_signal.dart';
+import 'package:beatboks/state/displayname_signal.dart';
+import 'package:beatboks/state/lastvisit_signal.dart';
 import 'package:beatboks/theme/text_utils.dart';
 import 'package:beatboks/widgets/snackbars.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:signals/signals_flutter.dart';
 
+final Signal<bool> isEminemChecked = signal<bool>(false);
+final Signal<bool> isKanyeChecked = signal<bool>(false);
+final Signal<bool> isWolfChecked = signal<bool>(false);
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBody: true,
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: 8,
-          itemBuilder: (BuildContext context, int index) {
-            return songSelectList[index];
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Show a SnackBar.
-          Snacks.showErrorSnack(
-            context,
-            'This app is work in progress! Please check back soon.',
-          );
-        },
-        child: Text(
-          sCheckedSongs.watch(context).toString(),
-          style: TextUtils.fontXL,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 8,
-        child: Row(
-          children: <Widget>[
-            IconButton(
-              onPressed: () {
-                showModalBottomSheet<Widget>(
-                  showDragHandle: true,
-                  isScrollControlled: true,
-                  context: context,
-                  builder: (BuildContext context) {
-                    return const SettingsBottomSheet();
-                  },
-                );
-              },
-              icon: const FaIcon(FontAwesomeIcons.gear),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-List<SongSelectCard> songSelectList = <SongSelectCard>[
-  SongSelectCard(
-    title: 'Eminem',
-    subTitle: 'Till I Collapse',
-    onTap: () {},
-  ),
-  SongSelectCard(
-    title: 'Kanye West',
-    subTitle: 'POWER',
-    onTap: () {},
-  ),
-  SongSelectCard(
-    title: 'Otherwise',
-    subTitle: 'Soldiers',
-    onTap: () {},
-  ),
-  SongSelectCard(
-    title: 'Eminem',
-    subTitle: 'Till I Collapse',
-    onTap: () {},
-  ),
-  SongSelectCard(
-    title: 'Kanye West',
-    subTitle: 'POWER',
-    onTap: () {},
-  ),
-  SongSelectCard(
-    title: 'Otherwise',
-    subTitle: 'Soldiers',
-    onTap: () {},
-  ),
-  SongSelectCard(
-    title: 'Eminem',
-    subTitle: 'Till I Collapse',
-    onTap: () {},
-  ),
-  SongSelectCard(
-    title: 'Kanye West',
-    subTitle: 'POWER',
-    onTap: () {},
-  ),
-  SongSelectCard(
-    title: 'Otherwise',
-    subTitle: 'Soldiers',
-    onTap: () {},
-  ),
-];
-
-class SongSelectCard extends StatefulWidget {
-  const SongSelectCard({
-    required this.title,
-    required this.subTitle,
-    required this.onTap,
-    super.key,
-  });
-
-  final String title;
-  final String subTitle;
-  final VoidCallback onTap;
-
-  @override
-  State<SongSelectCard> createState() {
-    return _SongSelectCardState();
-  }
-}
-
-class _SongSelectCardState extends State<SongSelectCard> {
-  bool isChecked = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          children: <Widget>[
-            ListTile(
-              onTap: widget.onTap,
-              leading: const FaIcon(FontAwesomeIcons.circlePlay),
-              title: Text(widget.title),
-              subtitle: Text(widget.subTitle),
-              trailing: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    isChecked = !isChecked;
-                    if (isChecked) {
-                      sCheckedSongs.value++;
-                    } else {
-                      sCheckedSongs.value--;
-                    }
-                  });
-                },
-                child: isChecked
-                    ? const FaIcon(FontAwesomeIcons.circleCheck)
-                    : const FaIcon(FontAwesomeIcons.circle),
+    return SafeArea(
+      child: Scaffold(
+        extendBody: true,
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: [
+                  Expanded(
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Column(
+                          children: <Widget>[
+                            const FaIcon(FontAwesomeIcons.userLarge),
+                            Text(cDisplayName.watch(context)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Expanded(
+                      child: Card(
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        children: <Widget>[
+                          FaIcon(FontAwesomeIcons.barsProgress),
+                          Text('Statistics'),
+                        ],
+                      ),
+                    ),
+                  )),
+                  Expanded(
+                      child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        children: <Widget>[
+                          const FaIcon(FontAwesomeIcons.calendarCheck),
+                          Text(sLastVisit.watch(context)),
+                        ],
+                      ),
+                    ),
+                  )),
+                ],
               ),
-            ),
-          ],
+              const Divider(thickness: 2),
+              ScrollConfiguration(
+                behavior: const ScrollBehavior().copyWith(
+                  scrollbars: false,
+                  dragDevices: <PointerDeviceKind>{
+                    PointerDeviceKind.touch,
+                    PointerDeviceKind.mouse,
+                    PointerDeviceKind.trackpad,
+                    PointerDeviceKind.stylus,
+                    PointerDeviceKind.invertedStylus,
+                  },
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: ListTile(
+                            leading: const FaIcon(FontAwesomeIcons.circlePlay),
+                            title: const Text('Eminem - Till I Collapse'),
+                            subtitle: const Text('TUTORIAL WORKOUT'),
+                            trailing: IconButton(
+                              onPressed: () {
+                                isEminemChecked.value = !isEminemChecked.value;
+                                if (isEminemChecked.value == true) {
+                                  sCheckedSongs.value++;
+                                } else {
+                                  sCheckedSongs.value--;
+                                }
+                              },
+                              icon: isEminemChecked.watch(context)
+                                  ? const FaIcon(FontAwesomeIcons.circleCheck)
+                                  : const FaIcon(FontAwesomeIcons.circle),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: ListTile(
+                            leading: const FaIcon(FontAwesomeIcons.circlePlay),
+                            title: const Text('Kanye West - Power'),
+                            subtitle: const Text('ALL DAY JABS'),
+                            trailing: IconButton(
+                              onPressed: () {
+                                isKanyeChecked.value = !isKanyeChecked.value;
+                                if (isKanyeChecked.value == true) {
+                                  sCheckedSongs.value++;
+                                } else {
+                                  sCheckedSongs.value--;
+                                }
+                              },
+                              icon: isKanyeChecked.watch(context)
+                                  ? const FaIcon(FontAwesomeIcons.circleCheck)
+                                  : const FaIcon(FontAwesomeIcons.circle),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: ListTile(
+                            leading: const FaIcon(FontAwesomeIcons.circlePlay),
+                            title: const Text('Masked Wolf - Astronaut In The'
+                                ' Ocean'),
+                            subtitle: const Text('HOOKS GALORE'),
+                            trailing: IconButton(
+                              onPressed: () {
+                                isWolfChecked.value = !isWolfChecked.value;
+                                if (isWolfChecked.value == true) {
+                                  sCheckedSongs.value++;
+                                } else {
+                                  sCheckedSongs.value--;
+                                }
+                              },
+                              icon: isWolfChecked.watch(context)
+                                  ? const FaIcon(FontAwesomeIcons.circleCheck)
+                                  : const FaIcon(FontAwesomeIcons.circle),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Show a SnackBar.
+            Snacks.showErrorSnack(
+              context,
+              'This app is work in progress! Please check back soon.',
+            );
+          },
+          child: Text(
+            sCheckedSongs.watch(context).toString(),
+            style: TextUtils.fontXL,
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 8,
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                onPressed: () {
+                  showModalBottomSheet<Widget>(
+                    showDragHandle: true,
+                    isScrollControlled: true,
+                    context: context,
+                    builder: (BuildContext context) {
+                      return const SettingsBottomSheet();
+                    },
+                  );
+                },
+                icon: const FaIcon(FontAwesomeIcons.gear),
+              ),
+            ],
+          ),
         ),
       ),
     );
