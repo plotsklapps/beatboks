c3241c60b486bdfd860dd5ea37730d70480d144f
plotsklapps <plotsklapps@gmail.com>
2024-04-08 13:34:40 +0200

    - Refactored summary_bottomsheet.dart to initialize the playList first.

--- lib/modals/summary_bottomsheet.dart
+++ lib/modals/summary_bottomsheet.dart
@@ -6,6 +6,7 @@ import 'package:beatboks/widgets/snackbars.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:logger/logger.dart';
 import 'package:signals/signals_flutter.dart';
 
 class SummaryBottomsheet extends StatefulWidget {
@@ -19,6 +20,49 @@ class SummaryBottomsheet extends StatefulWidget {
 
 class _SummaryBottomsheetState extends State<SummaryBottomsheet> {
   final AudioPlayer _audioPlayer = AudioPlayer();
+  final Signal<bool> sIsPlaying = signal<bool>(false);
+
+  @override
+  void initState() {
+    super.initState();
+    _initAudioPlayer();
+  }
+
+  Future<void> _initAudioPlayer() async {
+    // Set the assets list to the player on initialization.
+    try {
+      final List<AudioSource> playList =
+          sCheckedSongList.watch(context).map((Song song) {
+        return AudioSource.asset(
+          'assets/MP3/${song.artist} - ${song.title}.mp3',
+        );
+      }).toList();
+
+      await _audioPlayer.setAudioSource(
+        ConcatenatingAudioSource(children: playList),
+      );
+    } catch (e) {
+      Logger().e('Error: $e');
+    }
+
+    // playerState.playing returns a bool that is stored in the sIsPlaying
+    // signal. This signal is then used to update the UI.
+    _audioPlayer.playerStateStream.listen((PlayerState playerState) {
+      if (playerState.processingState == ProcessingState.ready) {
+        sIsPlaying.value = playerState.playing;
+      }
+      if (playerState.processingState == ProcessingState.completed) {
+        sIsPlaying.value = false;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    Logger().i('Audio player disposed.');
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,24 +110,15 @@ class _SummaryBottomsheetState extends State<SummaryBottomsheet> {
                 FloatingActionButton(
                   onPressed: () async {
                     if (sCheckedSongList.watch(context).isEmpty) {
-                      Snacks.showErrorSnack(
-                          context,
-                          'Please add some songs '
-                          'first, we recommend 3-8 songs for a solid workout!');
+                      Snacks.showErrorSnack(context,
+                          'Please add some songs first, we recommend 3-8 songs for a solid workout!');
                     } else {
-                      final List<AudioSource> playList =
-                          sCheckedSongList.watch(context).map((Song song) {
-                        return AudioSource.asset(
-                            'assets/MP3/${song.artist} - ${song.title}.mp3');
-                      }).toList();
-
-                      await _audioPlayer.setAudioSource(
-                        ConcatenatingAudioSource(children: playList),
-                      );
                       await _audioPlayer.play();
                     }
                   },
-                  child: const FaIcon(FontAwesomeIcons.play),
+                  child: sIsPlaying.watch(context)
+                      ? const FaIcon(FontAwesomeIcons.circlePause)
+                      : const FaIcon(FontAwesomeIcons.forwardStep),
                 ),
               ],
             ),
