ace066e8b4447a6bd0ba146d262b90d58c614ab3
plotsklapps <plotsklapps@gmail.com>
2024-04-13 16:30:13 +0200

    - Added workout_screen.dart scaffolding.

--- lib/navigation/navigation.dart
+++ lib/navigation/navigation.dart
@@ -1,11 +1,13 @@
 import 'package:beatboks/screens/home_screen.dart';
 import 'package:beatboks/screens/start_screen.dart';
+import 'package:beatboks/screens/workout_screen.dart';
 import 'package:flutter/material.dart';
 
 // Class dedicated to navigational Strings to avoid typo's.
 class NavString {
   static String startScreen = '/startScreen';
-  static String homeScreen = '/home';
+  static String homeScreen = '/homeScreen';
+  static String workoutScreen = '/workoutScreen';
 }
 
 // Defining routes with key-value pairs to use in the Navigate class.
@@ -15,6 +17,9 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   },
   NavString.homeScreen: (BuildContext context) {
     return const HomeScreen();
+  },
+  NavString.workoutScreen: (BuildContext context) {
+    return const WorkoutScreen();
   },
 };
 
@@ -26,5 +31,9 @@ class Navigate {
 
   static void toHomeScreen(BuildContext context) {
     Navigator.pushNamed(context, NavString.homeScreen);
+  }
+
+  static void toWorkoutScreen(BuildContext context) {
+    Navigator.pushNamed(context, NavString.workoutScreen);
   }
 }
---
+++ lib/screens/workout_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorkoutScreen extends StatefulWidget {
+  const WorkoutScreen({super.key});
+
+  @override
+  State<WorkoutScreen> createState() => _WorkoutScreenState();
+}
+
+class _WorkoutScreenState extends State<WorkoutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
